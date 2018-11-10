ecf1e03e98f1ab7b1c446366b5c5045b8148fa01
Aurélien Joubert <aujou@tohero.fr>
2018-11-10 15:58:11 +0100

    Add styles

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
+var colors = [
+    '#e8e458', //yellow
+    '#ea7d7d', //red
+    '#7daeea', //blue
+    '#89e079', //green
+    '#de8be8', //purple
+];
+
 var TodoItem = {
     props: ['item'],
-    template: '<li>{{ item.text }}</li>'
+    template: `<li>{{ item.text }}<button class="btn btn-link" @click="$emit('delete-row')"><i class="fa fa-times"></i></button></li>`
 };
 
 Vue.component('todo-list', {
     props: ['list'],
+    created: function() {
+        this.color = colors[Math.floor(Math.random() * Math.floor(colors.length))];
+    },
     template: `
-        <div>
-            <h1>Todo list #{{list.id}}</h1>
+        <div class="todolist col-4" v-bind:style="{ background: color}">
+            <h2>Todo list #{{list.id}}</h2>
+            <button class="btn btn-dark btn-close" @click="$emit('delete-row')"><i class="fa fa-times"></i></button>
             <ol>
                 <todo-item
-                    v-for="item in list.data"
+                    v-for="(item, index) in list.data"
                     v-bind:item="item"
-                    v-bind:key="item.id">
+                    v-bind:key="item.id"
+                    v-on:delete-row="deleteThisRow(index)">
                 </todo-item>
-                <input type="text" ref="input"/><button v-on:click="add">Add item</button>
+                <form class="input-group mb-3">
+                    <input type="text" ref="input" class="form-control">
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-dark" type="submit" v-on:click="add"><i class="fa fa-plus"></i></button>
+                    </div>
+                </form>
             </ol>
         </div>`,
     methods: {
         add: function (event) {
+            event.preventDefault();
             this.list.data.push({ text: this.$refs.input.value });
             this.$refs.input.value = '';
+        },
+        deleteThisRow: function(index) {
+            this.list.data.splice(index, 1);
         }
     },
     components: {
@@ -44,6 +66,9 @@ var app = new Vue({
             if (this.count > 1) {
                 this.count--;
             }
+        },
+        deleteThisRow: function(index) {
+            this.todoLists.splice(index, 1);
         }
     }
 });
